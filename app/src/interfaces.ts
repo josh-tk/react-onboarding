7cee7b57308383d39f4b8b5ece9d5b7f8200ed60
Josh Tait <josh.tait@travelperk.com>
2024-10-17 14:23:21 +0100

    finalise types, interfaces and schemas

--- app/src/interfaces.ts
+++ app/src/interfaces.ts
@@ -1,35 +1,107 @@
-import {BaseRecipeSchema, IdentifierSchema, ShowRecipeSchema} from "./schemas";
 import {InferType as Schema} from "yup";
+import {CreateRecipeSchema, IdentifierSchema, ShowRecipeSchema, UpdateRecipeSchema} from "./schemas";
 
 /**
  * Interfaces
- * BaseRecipeType: used for the initial values of any forms and creation of records
- * RecipeType: used for everything else (displaying, updating, deleting etc)
- * OptionType: used for select dropdowns, this is the shape of each option
+ * Types that are used in the application.
+ * They are built from Yup objects. See schemas.js.
  *
- * Both IngredientType and AuthorType are essentially the same as IdentifierType, but we're keeping them separate
- * for semantics. They could easily be decoupled if needed
- *
- * We leverage Yup's `InferType` so we can essentially have an interface with validation.
- * Docs: https://github.com/jquense/yup?tab=readme-ov-file#typescript-integration
- * See the `schemas.ts` file for the actual property definitions.
  */
 
-interface IngredientType extends Schema<typeof IdentifierSchema> {
-}
-
-interface AuthorType extends Schema<typeof IdentifierSchema> {
-}
-
-interface BaseRecipeType extends Schema<typeof BaseRecipeSchema> {
-}
-
+/**
+ * @see ShowRecipeSchema
+ * @property {string} id
+ * @property {string} name
+ * @property {IdentifierSchema[]} ingredients
+ * @property {IdentifierSchema} author
+ */
 interface ShowRecipeType extends Schema<typeof ShowRecipeSchema> {
 }
 
+/**
+ * @see CreateRecipeSchema
+ * @param {string} id - unique identifier
+ * @param {string} name - name of the recipe
+ * @param {IdentifierSchema[]} ingredients - list of ingredients
+ * @param {string} author_id - unique identifier of the author
+ */
+interface CreateRecipeType extends Schema<typeof CreateRecipeSchema> {
+}
+
+/**
+ * @see UpdateRecipeSchema
+ * @param {string} id - unique identifier
+ * @param {string} name - name of the recipe
+ * @param {IdentifierSchema[]} ingredients - list of ingredients
+ * @param {string} author_id - unique identifier of the author
+ */
+interface UpdateRecipeType extends Schema<typeof UpdateRecipeSchema> {
+}
+
+/**
+ * @see IdentifierSchema
+ * @param {string} id - unique identifier
+ * @param {string} name - name of the ingredient
+ */
+interface IngredientType extends Schema<typeof IdentifierSchema> {
+}
+
+/**
+ * Generic type for an identifier.
+ * @see IdentifierSchema
+ * @param {string} id - unique identifier
+ * @param {string} name - name of the ingredient
+ */
+interface IdentifierType extends Schema<typeof IdentifierSchema> {
+}
+
+/**
+ * Used in the SelectInput component.
+ * @property {string} value - value of the option
+ * @property {string} label - label of the option
+ *
+ * Maps from the IngredientType interface (value = id, label = name)
+ */
 interface OptionType {
     value: string;
     label: string;
 }
 
-export type {BaseRecipeType, ShowRecipeType, IngredientType, AuthorType, OptionType};
+/**
+ * @see IdentifierSchema
+ * @param {string} id - unique identifier
+ * @param {string} name - name of the author
+ */
+interface AuthorType extends Schema<typeof IdentifierSchema> {
+}
+
+/**
+ * Defines the actual shape of the form state
+ * Can't use the CreateRecipeType interface here because Formik doesn't support interfaces
+ * as initial values.
+ */
+interface CreateRecipeForm {
+    name: string;
+    ingredients: IdentifierType[];
+    author_id: string;
+}
+
+/**
+ * Based on the CreateRecipeForm interface, but includes the id field.
+ * @see CreateRecipeForm
+ */
+interface UpdateRecipeForm extends CreateRecipeForm {
+    id: string;
+}
+
+export type {
+    ShowRecipeType,
+    CreateRecipeType,
+    UpdateRecipeType,
+    IngredientType,
+    IdentifierType,
+    OptionType,
+    AuthorType,
+    CreateRecipeForm,
+    UpdateRecipeForm
+};
--- app/src/schemas.js
+++ app/src/schemas.js
@@ -2,45 +2,77 @@ import * as yup from 'yup';
 
 /**
  * Schemas
- * BaseRecipeSchema: schema for creating a new recipe only
- * RecipeSchema: everything else (presenting, updating, deleting)
- * IdentifierSchema: generic schema used to build Ingredient and Author objects
+ * Defined as Yup objects to validate form data
+ * Also used to define the structure of objects in the app
+ * See interfaces.ts for the types that these schemas represent
  *
- * These schemas will be used as the basis for the Interfaces
- * as well as client-side form validation, all in one.
- *
- * If you're not sure which to use, use RecipeSchema. Any saved record will have an ID.
+ * I've added doc blocks to each schema so you can see the shape
+ * in various contexts. This is useful for understanding the structure
+ * because they're built up from smaller schemas.
  */
 
+/**
+ * IdentifierSchema
+ * @param {string} id - unique identifier
+ * @param {string} name - name of the object
+ * @returns {object} - yup schema
+ * id and name are used so often in this app that it's worth making a schema for them
+ */
 let IdentifierSchema = yup.object({
     id: yup.string().required(),
     name: yup.string().required()
 });
 
+/**
+ * ShowRecipeSchema - Base template (not used directly)
+ * @param {string} id - unique identifier
+ * @param {string} name - name of the recipe
+ *
+ * Not used anywhere explicitly but useful for defining the structure
+ */
 let BaseRecipeSchema = yup.object({
     name: yup.string().required(),
     ingredients: yup.array().of(IdentifierSchema).required(),
-    author: IdentifierSchema.required(),
 });
 
-let ShowRecipeSchema = yup.object({
-    id: yup.string().required(),
-    name: yup.string().required(),
-    ingredients: yup.array().of(IdentifierSchema).required(),
-    author: IdentifierSchema.required(),
-});
+/**
+ * ShowRecipeSchema - Used when showing a recipe to the user
+ * @param {string} id - unique identifier
+ * @param {string} name - name of the recipe
+ * @param {IdentifierSchema} author - author object
+ */
+let ShowRecipeSchema = BaseRecipeSchema.concat(
+    yup.object({
+        id: yup.string().required(),
+        author: IdentifierSchema.required(),
+    })
+);
 
-let CreateRecipeSchema = yup.object({
-    name: yup.string().required(),
-    ingredients: yup.array().of(IdentifierSchema).required(),
-    author_id: yup.string().required(),
-});
+/**
+ * CreateRecipeSchema - Used for creating recipes
+ *
+ * @param {string} name - name of the recipe
+ * @param {IdentifierSchema[]} ingredients - list of ingredients
+ * @param {string} author_id - unique identifier of the author
+ */
+let CreateRecipeSchema = BaseRecipeSchema.concat(
+    yup.object({
+        author_id: yup.string().required(),
+    })
+);
 
-let UpdateRecipeSchema = yup.object({
-    id: yup.string().required(),
-    name: yup.string().required(),
-    ingredients: yup.array().of(IdentifierSchema).required(),
-    author_id: yup.string().required(),
-})
+/**
+ * UpdateRecipeSchema - Used for updating recipes
+ * @param {string} id - unique identifier
+ * @param {string} name - name of the recipe
+ * @param {IdentifierSchema[]} ingredients - list of ingredients
+ * @param {string} author_id - unique identifier of the author
+ */
+let UpdateRecipeSchema = BaseRecipeSchema.concat(
+    yup.object({
+        id: yup.string().required(),
+        author_id: yup.string().required(),
+    })
+);
 
 export {IdentifierSchema, ShowRecipeSchema, BaseRecipeSchema, CreateRecipeSchema, UpdateRecipeSchema};
