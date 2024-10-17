4efa250b540b21f585ffa742ce315ebe595c82b2
Josh Tait <josh.tait@travelperk.com>
2024-10-17 11:48:12 +0100

    add basic delete functionality

--- app/src/components/Recipe.tsx
+++ app/src/components/Recipe.tsx
@@ -1,8 +1,18 @@
 import React, {ReactElement, ReactNode} from 'react';
 import {IngredientType, ShowRecipeType} from "../interfaces";
 import Ingredient from "./Ingredient";
+import axios from "axios";
 
 const Recipe = ({recipe}: { recipe: ShowRecipeType }): ReactElement => {
+    const handleDelete = (id: string): void => {
+        axios.delete(`${process.env.REACT_APP_BACKEND_API_HOST}/recipes/${id}`)
+            .then((response) => {
+                console.log(response);
+            }).catch((error) => {
+            console.error(error);
+        });
+    };
+
     return (
         <div className="recipe">
             <h2>{recipe.name}</h2>
@@ -15,6 +25,7 @@ const Recipe = ({recipe}: { recipe: ShowRecipeType }): ReactElement => {
                 ))}
             </ul>
             <a href={`/recipes/update/${recipe.id}`}>Edit</a>
+            <button type={'button'} onClick={(): void => handleDelete(recipe.id)}>Delete</button>
         </div>
     );
 };
